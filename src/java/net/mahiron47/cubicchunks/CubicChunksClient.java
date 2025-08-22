1742acca817f6e33b30be5f005bd9b7d9724c7c3
Mahiron47 <faderotate@gmail.com>
2025-08-22 15:54:44 +0300

    ini

---
+++ src/java/net/mahiron47/cubicchunks/CubicChunksClient.java
@@ -0,0 +1,10 @@
+package net.mahiron47.cubicchunks;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CubicChunksClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// Client-side initialization code
+	}
+}
---
+++ src/java/net/mahiron47/cubicchunks/CubicChunksDataGenerator.java
@@ -0,0 +1,11 @@
+package net.mahiron47.cubicchunks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CubicChunksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
